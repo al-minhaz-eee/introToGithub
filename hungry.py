5c8428fce2e6145816a435906129f083555aa8fd
alminhaz04 <alminhaz04@gmail.com>
2024-07-15 07:28:08 +0600

    water

--- hungry.py
+++ hungry.py
@@ -1,7 +1,9 @@
 print("I am not hungry, I want to marry")
-print("do you marry me?")
 hungry = input("Do you marry me?")
-if hungry = 'yes':
+if hungry == 'yes':
     print("Let's go to marriage registry office")
 else:
     print("pass")
+
+print("I dont marry you because It is probitted 3 times,")
+print('by propet mohammad sw to marry without girl father')
