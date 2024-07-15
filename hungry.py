858cfd7a6a90cb48a268b81676ba19b1c41c3f7d
alminhaz04 <alminhaz04@gmail.com>
2024-07-15 07:16:15 +0600

    hungry for marrige

--- hungry.py
+++ hungry.py
@@ -1,2 +1,7 @@
 print("I am not hungry, I want to marry")
 print("do you marry me?")
+hungry = input("Do you marry me?")
+if hungry = 'yes':
+    print("Let's go to marriage registry office")
+else:
+    print("pass")
